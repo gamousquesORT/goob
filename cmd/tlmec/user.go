b0a2def77e1eb7f041f2969d7d196a4316510433
Gastón Mousqués <mousques@ort.edu.uy>
2022-09-18 17:33:20 +0000

    running user validations

--- cmd/tlmec/user.go
+++ cmd/tlmec/user.go
@@ -1,21 +1,44 @@
 package domain
 
+import("errors")
+
 type UserData struct {
-	name string
-	email string
-	password string
+	Name string
+	Email string
+	Password string
+	IsAdmin bool
 }
 
 type User interface {
 	NewUser(name, email, password string) (UserData, error)
 }
 
+var ErrExistingEmail = errors.New("email already exists")
+var ErrInvalidUserName = errors.New("user name must be at least 10 characters long")
+
+var userEmails = map[string]UserData{}
+
 func NewUser(name string, email string, password string) (*UserData, error) {
-	ud := new (UserData)
-	ud.name = name
-	ud.email = email
-	ud.password = password
-	
-	return ud, nil
+	if !checkExistingEmail(email) && len(name) >= 10 && len(name) <= 20 {
+		ud := new (UserData)
+		ud.Name = name
+		ud.Email = email
+		ud.Password = password
+		ud.IsAdmin = false
+		userEmails[email] = *ud
+
+		return ud, nil
+
+	} else if len(name) < 10 || len(name) > 20 { 
+		return &UserData{}, ErrInvalidUserName
+	} else {	
+		return &UserData{}, ErrExistingEmail
+	}
 
 }
+
+func checkExistingEmail(email string) bool {
+	_, ok := userEmails[email]
+	return ok
+}
+
--- cmd/tlmec/user_test.go
+++ cmd/tlmec/user_test.go
@@ -7,15 +7,63 @@ import (
 	"streamapp.com/domain"
 )
 
-func TestUserInit(t *testing.T) {
-	t.Run("should return an empty User", func(t *testing.T) {
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+	if got == nil {
+		t.Fatal("didn't get an error but wanted one")
+	}
 
-		got, _ := domain.NewUser("name", "email", "password")
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
 
-		want := domain.UserData{}
+func assertNoError(t testing.TB, got error) {
+	t.Helper()
+	if got != nil {
+		t.Fatal("got an error but didn't want one")
+	}
+}
+
+
+func TestValidUser(t *testing.T) {
+
+	t.Run("Should return no error given a valid User", func(t *testing.T) {
+
+		got, err := domain.NewUser("uniquename123", "uniqueemail", "uniquepassword")
+
+		want := domain.UserData{"uniquename123", "uniqueemail", "uniquepassword"}
+	
 		if reflect.DeepEqual(got,want) {
 			t.Errorf("Expected %v user name, got %v", got, want)
 		}
+
+		assertNoError(t, err)
+	})
+	t.Run("Should return an error given an exitent email", func(t *testing.T) {
+		domain.NewUser("uniquename123", "gaston1@example.com", "uniquepassword")
+		_, err := domain.NewUser("uniquename222", "gaston1@example.com", "password")
+
+		
+		assertError(t, err, domain.ErrExistingEmail)
+
 	})
 
+	t.Run("Should return an error given a user name with less than 10 chars", func(t *testing.T) {
+		_, err := domain.NewUser("gaston", "gaston@example.com", "password")
+
+		assertError(t, err, domain.ErrInvalidUserName)
+
+	})
+
+	t.Run("Should return an error given a user name with less more than 20 chars", func(t *testing.T) {
+		_, err := domain.NewUser("123456789012345678901", "gaston@example.com", "password")
+
+		assertError(t, err, domain.ErrInvalidUserName)
+
+	})
 }
+
+func TestValidAdminUser(t *testing.T) {
+}
+
