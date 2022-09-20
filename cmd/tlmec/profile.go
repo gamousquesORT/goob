8fd0384f87eb3a23d0e089ba0bcee47cbf1fd742
Gastón Mousqués <mousques@ort.edu.uy>
2022-09-20 17:48:09 +0000

    added profile tests and got green

--- cmd/tlmec/profile.go
+++ cmd/tlmec/profile.go
@@ -1,20 +1,42 @@
 package domain
 
+import("errors"
 
-
-
+"strconv")
 
 type ProfileData struct {
 	Alias string
 	Pin int
+	Owner bool
 }
 
 
+var ErrEInvalidAlias = errors.New("alias should be greater than 1 and less 16")
+var ErrEInvalidPin = errors.New("alias should be of 5 digits")
 
-func NewProfile(alias string, pin int) (*ProfileData, error) {
+
+
+func NewProfile(alias string, pin int, own bool) (*ProfileData, error) {
 	val := new(ProfileData)
+	if len(alias) <1 || len(alias) > 15 {
+		return &ProfileData{}, ErrEInvalidAlias
+	}
+
+	s := strconv.Itoa(pin)
+	b := true
+	for _, c := range s {
+		if c < '0' || c > '9' {
+			b = false
+			break
+		}
+	}
+	if !b || len(s) != 5 {
+		return &ProfileData{},ErrEInvalidPin
+	}
+	
 	val.Alias = alias
 	val.Pin = pin
+	val.Owner = own
 
 	return  val,nil
 }
--- cmd/tlmec/profile_test.go
+++ cmd/tlmec/profile_test.go
@@ -7,13 +7,13 @@ import (
 )
 
 
-func TestValidProfile(t *testing.T) {
+func TestValidOwnerProfile(t *testing.T) {
 
-	t.Run("Should return no error creating new vald profile", func(t *testing.T) {
+	t.Run("Should return no error creating new valid profile", func(t *testing.T) {
 
-		got, err := domain.NewProfile("alias", 12345)
+		got, err := domain.NewProfile("alias", 12345, true)
 
-		want := domain.ProfileData{"alias", 12345}
+		want := domain.ProfileData{"alias", 12345, true}
 
 		if *got  != want {
 			t.Errorf("got %v , want %v", got, want)
@@ -23,5 +23,45 @@ func TestValidProfile(t *testing.T) {
 	})
 
 
+	t.Run("Should return an error creating new profile with short Alias", func(t *testing.T) {
+
+		_, err := domain.NewProfile("", 12345, true)
+
+		assertError(t, err, domain.ErrEInvalidAlias)
+	})
+
+	t.Run("Should return an error creating new profile with long Alias", func(t *testing.T) {
+
+		_, err := domain.NewProfile("1234567890123456", 12345, true)
+
+		assertError(t, err, domain.ErrEInvalidAlias)
+	})
+
+	t.Run("Should return an error creating new profile with short Pin", func(t *testing.T) {
+
+		_, err := domain.NewProfile("1234567890", 1234, true)
+
+		assertError(t, err, domain.ErrEInvalidPin)
+	})
+
+	t.Run("Should return an error creating new profile with long Pin", func(t *testing.T) {
+
+		_, err := domain.NewProfile("1234567890", 123456, true)
+
+		assertError(t, err, domain.ErrEInvalidPin)
+	})
+
+	t.Run("Should return an error creating new owner profile with false flag", func(t *testing.T) {
+
+		g, err := domain.NewProfile("1234567890", 12345, true)
+		got := g.Owner
+		want := true
+
+		if got != want {
+			t.Errorf("got %v , want %v", got, want)
+		}
+
+		assertNoError(t, err)
+	})
 
 }
