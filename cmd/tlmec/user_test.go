ae662a3c6f1e4f8db12f3dc5f039bb9a5f900c70
Gastón Mousqués <mousques@ort.edu.uy>
2022-09-21 20:09:25 +0000

    split user test file to user and admin

--- cmd/tlmec/user_test.go
+++ cmd/tlmec/user_test.go
@@ -85,17 +85,3 @@ func TestValidUser(t *testing.T) {
 	})
 
 }
-
-func TestValidAdminUser(t *testing.T) {
-	t.Run("Should return true given an Admin user ", func(t *testing.T) {
-		createAdminUser(t)
-		adminUser.SetAdmin(true)
-		got := adminUser.GetAmdin()
-		want := true
-
-		if got != want {
-			t.Errorf("got %v, want %v", got, want)
-		}
-
-	})
-}
