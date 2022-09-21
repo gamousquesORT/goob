530f2aad3fed85690871fab40773c790d916b68b
Gastón Mousqués <mousques@ort.edu.uy>
2022-09-21 17:58:46 +0000

    refactor user

--- cmd/tlmec/user.go
+++ cmd/tlmec/user.go
@@ -9,40 +9,48 @@ type UserData struct {
 	IsAdmin bool
 }
 
+const (
+	MaxValidName = 20
+	MinValidName = 10
+
+	MaxValidPassword = 30
+	MinValidPassword = 10
+)
 
 var ErrExistingEmail = errors.New("email already exists")
 var ErrInvalidUserName = errors.New("user name must have more than 10 and less than 20 chars")
 var ErrInvalidUserPassword = errors.New("user password should have more than 10 and less than 30 chars")
+var ErrUnknownUserError  = errors.New("unknown user error")
+
 
 var userEmails = map[string]UserData{}
 
+
 func NewUser(name string, email string, password string) (*UserData, error) {
-	if !checkExistingEmail(email) && chackValidaName(name) && checkValidPassword(password) {
-		ud := new (UserData)
-		ud.Name = name
-		ud.Email = email
-		ud.Password = password
-		ud.IsAdmin = false
-		userEmails[email] = *ud
-
-		return ud, nil
-
-	} else if len(name) < 10 || len(name) > 20 { 
+	if !checkValidaName(name) {
 		return &UserData{}, ErrInvalidUserName
-	} else if (len(password) < 10 || len(password) > 30) {
-		return &UserData{}, ErrInvalidUserPassword
-	} else {	
+	} else if checkExistingEmail(email) {
 		return &UserData{}, ErrExistingEmail
-	}
+	} else if !checkValidPassword(password) {
+		return &UserData{}, ErrInvalidUserPassword
+	} 
+	ud := new (UserData)
+	ud.Name = name
+	ud.Email = email
+	ud.Password = password
+	ud.IsAdmin = false
+	userEmails[email] = *ud
+
+	return ud, nil
 
 }
 
-func chackValidaName(name string) bool {
-	return len(name) >= 10 && len(name) <= 20
+func checkValidaName(name string) bool {
+	return len(name) >= MinValidName && len(name) <= MaxValidName
 }
 
 func checkValidPassword(password string) bool {
-	return len(password) >= 10 && len(password) <= 30
+	return len(password) >= MinValidPassword && len(password) <= MaxValidPassword
 }
 
 func checkExistingEmail(email string) bool {
