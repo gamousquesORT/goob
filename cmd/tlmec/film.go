bee35fb12e37f2c76ca68b40586b245cb59e0bf1
Gastón Mousqués <mousques@ort.edu.uy>
2022-09-23 16:25:17 +0000

    added some test for film before staritng geneers

--- cmd/tlmec/film.go
+++ cmd/tlmec/film.go
@@ -1,5 +1,7 @@
 package domain
 
+import "errors"
+
 type Rating int
 
 const (  
@@ -11,8 +13,15 @@ type FilmData struct {
 
 }
 
+
+var ErrFilmNameMissing  = errors.New("missing film name")
+
 //domain.NewFilmData("Matrix", interface{}, interface{}, "pelicula sci-fi buena parte 1", domain.G, true)
 func NewFilmData(name string, genres any, poster any, desc string, r Rating, spon bool) (*FilmData, error) {
+	if len(name) < 1 {
+		return &FilmData{}, ErrFilmNameMissing
+	}
+
 	fd := new(FilmData)
 	return fd, nil
 }
--- cmd/tlmec/film_test.go
+++ cmd/tlmec/film_test.go
@@ -17,4 +17,21 @@ func TestValidFilm(t *testing.T) {
 		assertNoError(t, err)
 	
 	})
+	
+	t.Run("Should return no error given a valid Film data", func(t *testing.T) {
+		got, err := domain.NewFilmData("Matrix", 1, 1, "pelicula sci-fi buena parte 1", domain.G, true)
+		want := domain.FilmData{}
+		if reflect.DeepEqual(got, want) {
+			t.Errorf("got %v , want %v", got, want)
+		}
+
+		assertNoError(t, err)
+	
+	})
+	
+	t.Run("Should return an error given a Film without name", func(t *testing.T) {
+		_, err := domain.NewFilmData("", 1, 1, "pelicula sci-fi buena parte 1", domain.G, true)
+	
+		assertError(t, err, domain.ErrFilmNameMissing)
+	})
 }
