e5f5c6582f387eca4b76beac205595f42c02400e
Gastón Mousqués <mousques@ort.edu.uy>
2022-09-27 14:51:16 +0000

    refactores some film test

--- cmd/tlmec/film_test.go
+++ cmd/tlmec/film_test.go
@@ -5,9 +5,48 @@ import ("testing"
 "reflect"
 )
 
-func TestValidFilm(t *testing.T) {
+func createValidFilm(t *testing.T) domain.FilmData {
+	t.Helper()
 
+	want := new(domain.FilmData)
+	g := domain.GenresData{"Terror", "da miedo"}
+	want.Name ="Matrix"
+	want.Descr = "pelicula sci-fi buena parte 1"
+	want.Rate = domain.G
+	want.Sponsored = true
+	want.Genres = make([]domain.FilmGenre, 4)
+	fg := domain.FilmGenre{}
+	fg.Gener = g
+	fg.GnType = domain.MainGenre
+	want.Genres[0] = fg
+	return *want
+}
+
+func TestValidFilm(t *testing.T) {
 	t.Run("Should return no error given a valid Film data", func(t *testing.T) {
+		g := domain.GenresData{"Terror", "da miedo"}
+		got, err := domain.NewFilmData("Matrix", g ,domain.MainGenre, "path to picture storage", "pelicula sci-fi buena parte 1", domain.G, true)
+		
+		want := createValidFilm(t)
+
+		if !reflect.DeepEqual(got, &want) {
+			t.Errorf("got %v , want %v", got, want)
+		}
+
+		assertNoError(t, err)
+	
+	})
+	
+
+	t.Run("Should return an error given a Film without name", func(t *testing.T) {
+		g := domain.GenresData{}
+		_, err := domain.NewFilmData("", g,domain.MainGenre,  "path to picture storage", "pelicula sci-fi buena parte 1", domain.G, true)
+	
+		assertError(t, err, domain.ErrFilmNameMissing)
+	})
+
+
+	t.Run("Should return an error given a secondary genres without a primary genre", func(t *testing.T) {
 		g := domain.GenresData{"Terror", "da miedo"}
 		got, err := domain.NewFilmData("Matrix", g ,domain.MainGenre, "path to picture storage", "pelicula sci-fi buena parte 1", domain.G, true)
 		
@@ -28,14 +67,6 @@ func TestValidFilm(t *testing.T) {
 
 		assertNoError(t, err)
 	
-	})
-	
-
-	t.Run("Should return an error given a Film without name", func(t *testing.T) {
-		g := domain.GenresData{}
-		_, err := domain.NewFilmData("", g,domain.MainGenre,  "path to picture storage", "pelicula sci-fi buena parte 1", domain.G, true)
-	
-		assertError(t, err, domain.ErrFilmNameMissing)
 	})
 
 	t.Run("Shoud return given a -1 rating it should return -1", func(t *testing.T) {
