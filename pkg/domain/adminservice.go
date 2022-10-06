6f84be17133a79cf99f1a9dafd2be7e40c00a777
Gastón Mousqués <mousques@ort.edu.uy>
2022-10-06 19:45:32 +0000

    admin services basic test green

--- pkg/domain/adminservice.go
+++ pkg/domain/adminservice.go
@@ -1,11 +1,17 @@
 package domain
 
+
+
 type StreamApp struct {
 	adminUser *UserData
+	genres []GenresData
 }
 
 
-var StreamAppData StreamApp
+func NewStreamApp() *StreamApp {
+	return new(StreamApp)
+}
+
 
 func (sta *StreamApp) CreateAdmin() (UserData, error) {
 	var err error
@@ -17,5 +23,16 @@ func (sta *StreamApp) CreateAdmin() (UserData, error) {
 	return *sta.adminUser, nil
 }
 
+
+func (sta *StreamApp) CreateGenre(name, desc string) (error) {
+	gen, err := NewGenresData(name, desc)
+	sta.genres = append(sta.genres, *gen)
+
+	return err
+}
+
+func (sta StreamApp) GetGenres() ([]GenresData) {
+	return sta.genres
+}
 
 
--- pkg/domain/adminservice_test.go
+++ pkg/domain/adminservice_test.go
@@ -2,14 +2,16 @@ package domain_test
 
 import (
 	"goob/app/pkg/domain"
+	"reflect"
 	"testing"
 )
 
 
+
 func TestAdminServices(t *testing.T) {
 	t.Run("Should create the default Admin User ", func(t *testing.T) {
-
-		admin, err := domain.StreamAppData.CreateAdmin()
+		var streamApp = domain.NewStreamApp()
+		admin, err := streamApp.CreateAdmin()
 		got := admin.IsAdmin
 		want := true 
 		if got != want {
@@ -18,6 +20,25 @@ func TestAdminServices(t *testing.T) {
 		assertNoError(t, err)
 		
 	})
+
+	t.Run("Should create a new genre with no error given valid data ", func(t *testing.T) {
+		var streamApp = domain.NewStreamApp()
+		 err := streamApp.CreateGenre("Terror", "Genero para tener miedo")
+
+		assertNoError(t, err)
+	})
+
+	t.Run("Should create a new genre and retrieve it ", func(t *testing.T) {
+		var streamApp = domain.NewStreamApp()
+		err := streamApp.CreateGenre("Terror", "Genero para tener miedo")
+		got := streamApp.GetGenres()
+		want := []domain.GenresData{{Name: "Terror", Description: "Genero para tener miedo"}}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %v , want %v", got, want)}
+
+	   assertNoError(t, err)
+   })
 
 
 }
